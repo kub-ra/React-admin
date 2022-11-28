edce8dd85bcc2c9934137c8f906fbe8da1dc0c68
kubramenziletoglu <kubra.menziletoglu@hellosmpl.com>
2022-11-29 02:54:54 +0300

    created react-admin

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => <Admin dataProvider={dataProvider} />;
+
+export default App;
